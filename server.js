c0592aaeac99ccbe461cc4df176a194d08051c93
rinaexplore <rinaexplore@gmail.com>
2020-02-28 16:01:36 -0500

    first ssr commit

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.prepare()
     server.get('*', (req,res) => {
         return handle(req,res);
     })
-
+    console.log(process.env)
     const port = process.env.PORT || 3000;
 
     server.listen(port, err => {
